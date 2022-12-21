d876f08af4b544da72a4f0fe199c2dac550eaa83
jeong hyewon <wonizizi89@naver.com>
2022-12-21 18:51:33 +0900

    아이디, 비밀번호 정규식 구현

--- src/main/java/com/example/myblog1/controller/UserController.java
+++ src/main/java/com/example/myblog1/controller/UserController.java
@@ -4,20 +4,13 @@ package com.example.myblog1.controller;
 import com.example.myblog1.dto.LoginRequest;
 import com.example.myblog1.dto.ResponseStatusDto;
 import com.example.myblog1.dto.SignupRequest;
-import com.example.myblog1.entity.User;
 //import com.example.myblog1.message.Message;
-import com.example.myblog1.message.StatusEnum;
 import com.example.myblog1.repository.UserRepository;
 import com.example.myblog1.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.Charset;
-import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,7 +22,7 @@ public class UserController {
 //    private org.springframework.http.ResponseEntity<Message> ResponseEntity;
 
 
-    //??? 포스트 맵핑이면 바디로 데이터가 들어오니 @requestBody 사용 하지 않나??
+
 
     @PostMapping("/signup")
     public ResponseStatusDto signup(@RequestBody SignupRequest signupRequest) {
@@ -44,7 +37,6 @@ public class UserController {
         //또는 return ResponseEntity.status(HttpStatus.OK).body(userService.login(loginRequestDto,response)); 간단히 표시가능
 
     }
-
 
 
 
@@ -67,9 +59,3 @@ public class UserController {
 
 }
 
-//이제는 ajax에서 body에 값이 넘어오기 때문에 이렇게 @RequestBody를 써주셔야 함
-
-//HttpServletResponse 는 우리가 HttpRequest에서 헤더가 넘어와 받아오는 것처럼
-//우리도 클라이언트쪾으로 반환 할 때는  이렇게 Response객체를 반환함
-//그래서 이것도 미리 가지고 와서 반환할 response Header에다가 우리가 만들어준 토큰을 넣이주기 위해서
-//이렇게 받아오고 있음
