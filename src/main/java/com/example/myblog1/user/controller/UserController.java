d9f1abab8f0743872dd23c78437a423eecf581e8
jeong hyewon <wonizizi89@naver.com>
2023-01-13 20:21:21 +0900

    회원탈퇴 구현

--- src/main/java/com/example/myblog1/user/controller/UserController.java
+++ src/main/java/com/example/myblog1/user/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.myblog1.user.controller;
 
 //import com.example.myblog1.message.Message;
 import com.example.myblog1.user.dto.LoginRequest;
+import com.example.myblog1.user.dto.ResignRequest;
 import com.example.myblog1.user.dto.ResponseStatusDto;
 import com.example.myblog1.user.dto.SignupRequest;
 import com.example.myblog1.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -30,10 +32,15 @@ public class UserController {
 
 
     @PostMapping("/login")
-    public ResponseStatusDto login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    public ResponseStatusDto login(@RequestBody @Valid LoginRequest loginRequest, HttpServletResponse response) {
         return userService.login(loginRequest, response);
         //또는 return ResponseEntity.status(HttpStatus.OK).body(userService.login(loginRequestDto,response)); 간단히 표시가능
 
+    }
+
+    @DeleteMapping("/resign/{id}")
+    public ResponseStatusDto resignMembership(@PathVariable Long id,@RequestBody ResignRequest resignRequest){
+        return userService.resignMembership(id,resignRequest);
     }
 
 }
--- src/main/java/com/example/myblog1/user/entity/StatusEnum.java
+++ src/main/java/com/example/myblog1/user/entity/StatusEnum.java
@@ -9,7 +9,8 @@ public enum StatusEnum {
     SIGNUP_SUCCESS(200,"회원가입 성공"),
     LOGIN_SUCCESS(200, "로그인 성공"),
 
-    POSTS_DELETE_SUCCESS(200,"게시글 삭제 성공");
+    POSTS_DELETE_SUCCESS(200,"게시글 삭제 성공"),
+    USER_DELETE_SUCCESS(200,"회원 탈퇴 성공" );
 
 
 
